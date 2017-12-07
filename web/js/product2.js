aaac6789a20431d51bf189aab6d21f060d801f2c
ThomasGroenewegen <thomasgroenewegen@hotmail.com>
2017-12-07 11:57:43 +0100

    Eerste opzet winkelwagen, geeft nu nog foutmelding tabulator bij toevoegen product aan winkelwagen

---
+++ web/js/product2.js
@@ -0,0 +1,125 @@
+var baseURL = "http://localhost:8080/Workshop3/webresources";
+
+var availableProducts;
+var selectedProducts = [];
+
+$(document).ready(function () {
+    parseShoppingCart();
+    console.log("parsing shopping cart finished");
+
+    showAllProducts();
+
+    console.log("selectedProducts length: " + selectedProducts.length);
+
+    for (var i = 0; i < selectedProducts.length; i++) {
+        console.log(selectedProducts[i]);
+    }
+
+    $(window).bind('beforeunload', function () {
+        console.log("Reloading");
+        storeShoppingCart();
+    });
+
+
+});
+
+function showAllProducts() {
+    $.ajax({
+        method: "GET",
+        url: baseURL + "/product/available",
+        dataType: "json",
+        error: function () {
+            console.log("error");
+        },
+        success: function (data) {
+            availableProducts = data;
+            setAvailable();
+            $("#productTable").tabulator({
+                layout: "fitColumns",
+                columns: [
+                    {title: "Product", field: "name", headerFilter: "input"},
+                    {formatter: "money", title: "Prijs/stuk", field: "price"},
+                    {title: "Voorraad", field: "stock"},
+//                    {title: "Aantal toe te voegen", field: "amount", editor: true, validator: ["numeric"]},
+                    {title: "Aantal toe te voegen", field: "amount", editable: true, editor: "number", validator: ["numeric"]},
+                    {formatter: "buttonTick", title: "Toevoegen", align: "center", cellClick: function (e, cell) {
+
+                            rowData = cell.getRow().getData();
+                            console.log(rowData.name);
+
+                            if (rowData.amount === undefined) {
+                                alert("U dient eerst een aantal in te voeren voordat u dit product kunt toevoegen");
+                                return;
+                            }
+                            if (rowData.amount < 1) {
+                                alert("Om een product toe te voegen dient u een minimumaantal van '1' in te voeren");
+                                return;
+                            }
+                            if (rowData.amount > rowData.stock) {
+                                alert("Het is niet mogelijk om meer producten te bestellen dan de voorraad bevat");
+                                return;
+                            }
+
+                            selectedProducts.push(cell.getRow().getData());
+                            console.log("Pushed to selectedProducts");
+                            rowData.chosen = "Reeds in winkelwagen";
+                            console.log("Chosen message updated");
+
+                            console.log("Selected products length" + selectedProducts.length);
+                            
+                            $("#productTable").tabulator("setData", availableProducts);
+                                                
+
+                        }},
+                    {title: "Status", field: "chosen"}
+                ]
+            });
+            availableProducts.sort(compare);
+            $("#productTable").tabulator("setData", availableProducts);
+
+        }
+    });
+}
+
+function setAvailable() {
+    for (var i = 0; i < availableProducts.length; i++) {
+        var availableRow = availableProducts[i];
+        availableRow.chosen = "Beschikbaar";
+
+        for (var j = 0; j < selectedProducts.length; j++) {
+            var selectedRow = selectedProducts[j];
+
+            if (availableRow.id === selectedRow.id) {
+                console.log("i is: " + i + " and j is " + j);
+                availableRow.chosen = "Reeds in winkelwagen";
+                break;
+            }
+        }
+    }
+}
+
+function parseShoppingCart() {
+    console.log("Entered unpacking method");
+    console.log("length: " + sessionStorage.shoppingCart.length);
+    if (sessionStorage.shoppingCart.length !== 0)
+        selectedProducts = JSON.parse(sessionStorage.getItem("shoppingCart"));
+    else
+        selectedProducts = [];
+}
+
+function storeShoppingCart() {
+    if (selectedProducts.length !== 0) {
+        sessionStorage.setItem("shoppingCart", JSON.stringify(selectedProducts));
+    } else {
+        sessionStorage.setItem("shoppingCart", JSON.stringify(""));
+    }
+
+}
+
+function compare(a, b) {
+    if (a.name < b.name)
+        return -1;
+    if (a.name > b.name)
+        return 1;
+    return 0;
+}
